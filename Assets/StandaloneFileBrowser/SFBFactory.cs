858f26ac84c8288349f608eab50df01d4166668c
Pavel Krasnov <krasnovpavel0@gmail.com>
2020-02-27 16:36:08 +0300

    Factory pattern implemented

---
+++ Assets/StandaloneFileBrowser/SFBFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace SFB
+{
+    public static class SFBFactory
+    {
+        public static IStandaloneFileBrowser Create()
+        {
+#if UNITY_STANDALONE_OSX
+            return new StandaloneFileBrowserMac();
+#elif UNITY_STANDALONE_WIN
+            return new StandaloneFileBrowserWindows();
+#elif UNITY_STANDALONE_LINUX
+            return new StandaloneFileBrowserLinux();
+#elif UNITY_EDITOR
+            return new StandaloneFileBrowserEditor();
+#endif
+        }
+    }
+}
